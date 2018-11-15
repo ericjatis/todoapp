2c19692224ae8b622bebccefcbc06750fc623ff4
Eric Halim <eric.halim@jatis.com>
2018-11-15 14:01:27 +0700

    Auto Config

--- todoapp/src/main/java/com/todoapp/TodoappApplication.java
+++ todoapp/src/main/java/com/todoapp/TodoappApplication.java
@@ -1,9 +1,13 @@
 package com.todoapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages="com.todoapp")
 public class TodoappApplication {
 
 	public static void main(String[] args) {
