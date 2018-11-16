f7c8c238c049432d59f82f4127d0bde7e5a83bcc
abdul <jalilabdul523@gmail.com>
2018-11-16 11:36:22 +0700

    test24

--- todoapp/src/main/java/com/todoapp/dao/UserDAO.java
+++ todoapp/src/main/java/com/todoapp/dao/UserDAO.java
@@ -3,6 +3,7 @@ package com.todoapp.dao;
 import java.util.List;
 import com.todoapp.model.User;
 //EricHalim
+//AbdulJalil1
 public interface UserDAO {
 	public void createUser(User user);
 	public User findUserByUsername(String username);
